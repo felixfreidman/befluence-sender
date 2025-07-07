6bebc4ad0245cf148d2408bf089c151c73bb4bd0
Felix Freidman <felixfreidman@gmail.com>
2025-07-07 18:08:37 +0300

    store + react-dom setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router";
+import Main from "./pages/Main/Main";
 
 function App() {
-	return <div></div>;
+	return (
+		<div className="classes.App">
+			<Routes>
+				<Route path="/" element={<Main />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/features/Global/GlobalSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface Title {
+	value: string;
+}
+
+interface GlobalTitle {
+	title: string;
+}
+
+const initialState: GlobalTitle = {
+	title: "",
+};
+
+const globalSlice = createSlice({
+	name: "Global",
+	initialState,
+	reducers: {
+		setTitle: (state, action: PayloadAction<string>) => {
+			state.title = action.payload;
+		},
+	},
+});
+
+export const { setTitle } = globalSlice.actions;
+export default globalSlice.reducer;
